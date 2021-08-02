810a88103b4116f0f622ddd11816d09db95128b7
hugoyue <gowah.yue@gmail.com>
2021-08-02 11:51:58 +0000

    update:默认摘要为80字

--- includes/head.php
+++ includes/head.php
@@ -39,11 +39,11 @@ if (isset($_POST['void_action'])) {
     ?>
     <title><?php Contents::title($this); ?><?php if ($this->is('index')): ?><?php if (!empty($setting['subtitle'])): ?><?php echo ' - '.$setting['subtitle']; ?><?php endif; ?><?php endif; ?></title>
     <meta name="author" content="<?php $this->author(); ?>" />
-    <meta name="description" content="<?php if($description != '') echo $description; else $this->excerpt(50); ?>" />
+    <meta name="description" content="<?php if($description != '') echo $description; else $this->excerpt(80); ?>" />
     <?php $this->header('commentReply=&description=&wlw=&xmlrpc=&rss2=&atom=&rss1=&template=&pingback=&generator'); ?>
     <?php if($this->is('post') || $this->is('page')) {?>
     <meta property="og:title" content="<?php Contents::title($this); ?>" />
-    <meta property="og:description" content="<?php if($description != '') echo $description; else $this->excerpt(50); ?>" />
+    <meta property="og:description" content="<?php if($description != '') echo $description; else $this->excerpt(80); ?>" />
     <meta property="og:site_name" content="<?php Contents::title($this); ?>" />
     <meta property="og:type" content="<?php if($this->is('post') || $this->is('page')) echo 'article'; else echo 'website'; ?>" />
     <meta property="og:url" content="<?php $this->permalink(); ?>" />
