64b8abab6e76690c924339066b3b088b391f8d56
hugoyue <hugoyue@users.noreply.github.com>
2021-08-09 17:32:42 +0000

    Automated deployment: Mon Aug  9 17:32:42 UTC 2021 1f2c7a66875bfd9b538c477dfe1d3d248bae7ea6

--- includes/head.php
+++ includes/head.php
@@ -72,7 +72,7 @@ else {
 
     <!--CSS-->
     <link rel="stylesheet" href="<?php echo $assetsUrl.'/bundle-1e9bf597b1.css'; ?>">
-    <link rel="stylesheet" href="<?php echo $assetsUrl.'/VOID-cb58f480bc.css'; ?>">
+    <link rel="stylesheet" href="<?php echo $assetsUrl.'/VOID-f0a753d26d.css'; ?>">
 
     <!--JS-->
     <script src="<?php echo $assetsUrl.'/bundle-header-9977c8fbf5.js'; ?>"></script>
